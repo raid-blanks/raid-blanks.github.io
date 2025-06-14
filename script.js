fb362c5637e4271d74240cd4aef6e8cbab3fea67
raid-blanks <mafpeo@upgradeInc.us>
2025-06-13 21:50:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,14 @@ function loadRaidClips() {
         card.className = 'clip-card';
         card.innerHTML = `
           <h3>${raid.title}</h3>
-          <video src="${raid.video}" controls loop></video>
+          <div class="clip-video">
+            <iframe 
+              src="${raid.video}" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen>
+            </iframe>
+          </div>
         `;
         container.appendChild(card);
       });
